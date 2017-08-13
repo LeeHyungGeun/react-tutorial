1871d7bababb147fbbf5fdba58f8ef06d1ca24d9
LeeHyungGeun <leehyunggeun@navercorp.com>
2017-08-13 18:05:12 +0800

    ready

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class HelloWorldComponent extends React.Component {
-    render() {
-        return <div>Hello World</div>;
-    }
-}
-ReactDOM.render(<HelloWorldComponent />, document.getElementsByTagName('body')[0]);
